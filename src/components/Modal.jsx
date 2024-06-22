fb837b195509691348c075df82c10e596a2fabea
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-06-22 05:00:00 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,11 +4,16 @@ import React from "react";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Modal = () => {
+const Modal = ({ modalToggle }) => {
   return (
     <div className="ModalBg">
       <div className="ModalContainer">
-        <FontAwesomeIcon icon={faXmark} />
+        <FontAwesomeIcon icon={faXmark} onClick={modalToggle} />
+        <div className="ModalSuccess">
+          <FontAwesomeIcon icon={faCheck} />
+        </div>
+        <h2>Payment successful</h2>
+        <p>Your order is on the way :)</p>
       </div>
     </div>
   );
